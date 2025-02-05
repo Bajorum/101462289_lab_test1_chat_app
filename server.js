a10b41d12272f2b0774d585598ae33f4d66a1914
Bajorum <Bilal@Youssef.ca>
2025-02-05 17:31:57 -0500

    server.js fixed

--- server.js
+++ server.js
@@ -100,6 +100,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start the Server
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
